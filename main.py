51a466a710b91f2ca242ecdac4e18b6d889bb4b2
denizzsarierr <denizsarier41950@hotmail.com>
2025-10-22 23:05:11 +0200

    Store distance data in DataFrame

--- main.py
+++ main.py
@@ -8,19 +8,25 @@ class City:
         self.id = int(id)
         self.x_coord = float(x_coord)
         self.y_coord = float(y_coord)
+        self.distances = []
         
     def __str__(self):
         
         return f"ID : {self.id}, X : {self.x_coord}, Y : {self.y_coord}"
 
-
+    # Finding distance between two cities. We use it in our Parser class.
     def find_distance(self,compare_city):
 
         distance = ((self.x_coord - compare_city.x_coord)**2 + (self.y_coord - compare_city.y_coord)**2)**0.5
+        self.distances.append((compare_city.id,distance))
 
         return distance
 
-
+    def convert_frame(self):
+        
+        df = pd.DataFrame([{"FC": self.id, "SC": c[0], "DISTANCE": c[1]} for c in self.distances])
+        df.drop_duplicates(inplace = True)
+        return df
 
 class Parser:
 
@@ -29,7 +35,10 @@ class Parser:
         self.dataset_info = {}
         self.city_data = []
         self.path = file_path
+        self.distance_df = []
         self._convert_frame()
+        # Call the parsing and converting function when the object is created.
+
 
     def _parse_date(self):
 
@@ -66,13 +75,29 @@ class Parser:
                     city = City(details[0],details[1],details[2])
                 
                     self.city_data.append(city)
-            
+
+    # Converting our list(data we got from tsp file) to a DataFrame.
     def _convert_frame(self):
 
         self._parse_date()
         df = pd.DataFrame([{"id": c.id, "x_coordinate": c.x_coord, "y_coordinate": c.y_coord} for c in self.city_data])
         df.drop_duplicates(inplace = True)
         return df
+
+    def city_dataframe(self):
+
+        # Here we use the find_distance method of City class to find distances between each city.
+        temp_distances = []
+        for c1 in self.city_data:
+            for c2 in self.city_data:
+                # To avoid duplicate
+                if c2.id > c1.id:
+                    distance = c1.find_distance(c2)
+                    temp_distances.append((c1.id,c2.id,distance))
+
+        self.distance_df = pd.DataFrame(temp_distances,columns=["FCID","SCID","Distance"])
+        return self.distance_df
+    
 
 
 def parse_data(file_name):
@@ -123,13 +148,18 @@ def parse_data(file_name):
 
 if __name__ == "__main__":
 
-    file_Name = 'berlin11_modified.tsp'
+    file_b11 = 'berlin11_modified.tsp'
+    file_b52 = 'berlin52.tsp'
+    pd.set_option('display.max_rows', None)
 
-    data = Parser(file_path = file_Name)
+    data = Parser(file_path = file_b52)
 
-    for d in data.city_data:
-        for a in data.city_data:
-            if d.id != a.id:
-                print(f"Distance between City {d.id} and City {a.id} is {d.find_distance(a)}")
+    dataset_info = data.dataset_info
+    distances = data.city_dataframe()
+    
+    print(distances)
 
+
+    
+   
     
